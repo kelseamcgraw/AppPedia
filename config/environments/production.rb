44f37a5673a885e8d49bf31e134cb70351732d21
kelseamcgraw <mcgrawkr@miamioh.edu>
2017-11-04 21:32:58 -0400

    user account gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #Required for devise gem
+  config.action_mailer.default_url_options = { host: 'http://apppedia.herokuapp.com', port: 3000 }
+  
 end
